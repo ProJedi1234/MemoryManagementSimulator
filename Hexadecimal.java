b0c84a3ec806799af19f5b07846cec961a6a01aa
Aditya Dhar <adhar2032@gmsil.vom>
2021-03-09 20:07:55 -0800

    Created hexadecimal file to work with addresses

---
+++ Hexadecimal.java
@@ -0,0 +1,16 @@
+public class Hexadecimal {
+    public final int pageFrame;
+    public final int offset;
+
+    public Hexadecimal(String hexValue) {
+        pageFrame = Integer.parseInt((String.valueOf(hexValue.charAt(0)) + String.valueOf(hexValue.charAt(1))), 16);
+        offset = Integer.parseInt((String.valueOf(hexValue.charAt(2)) + String.valueOf(hexValue.charAt(3))), 16);
+    }
+
+    public String pageFrameHex() {
+        return String.format("%02X", pageFrame);
+    }
+    public String offsetHex() {
+        return String.format("%02X", offset);
+    }
+}
